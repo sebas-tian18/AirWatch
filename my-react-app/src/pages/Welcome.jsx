05c56ef6e7d27f25f8169667c0de7e82b4114257
der-kaffe <cafeconcafeina@gmail.com>
2024-05-13 21:38:53 -0400

    colocando tailwind

--- my-react-app/src/pages/Welcome.jsx
+++ my-react-app/src/pages/Welcome.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ArrowComponent from '../Components/Toward';
 function Welcome() {
     return (
-        <div>
+        <div className='bg-cyan-500	'>
             <div><ArrowComponent to="/" /></div>
             <h1>Welcome to AirWach!</h1>
         </div>
